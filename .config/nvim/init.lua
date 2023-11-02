091073482ba0eb251f42dcc4eba3af0eb149260e
Maximilian Fernaldy <maximilian.fernaldy@gmail.com>
2023-11-03 03:59:42 +0900

    nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -109,6 +109,17 @@ require('lazy').setup({
     },
   },
 
+  {
+    "chikko80/error-lens.nvim",
+    event = "BufRead",
+    dependencies = {
+        "nvim-telescope/telescope.nvim"
+    },
+    opts = {
+        -- your options go here
+    },
+  },
+
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
   {
@@ -559,6 +570,75 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+-- My own configs
+
+vim.cmd([[
+  call plug#begin()
+  " The default plugin directory will be as follows:
+  "   - Vim (Linux/macOS): '~/.vim/plugged'
+  "   - Vim (Windows): '~/vimfiles/plugged'
+  "   - Neovim (Linux/macOS/Windows): stdpath('data') . '/plugged'
+  " You can specify a custom plugin directory by passing it as the argument
+  "   - e.g. `call plug#begin('~/.vim/plugged')`
+  "   - Avoid using standard Vim directory names like 'plugin'
+
+  " Make sure you use single quotes
+
+  " Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
+  Plug 'junegunn/vim-easy-align'
+
+  " Any valid git URL is allowed
+  Plug 'https://github.com/junegunn/vim-github-dashboard.git'
+
+  " Multiple Plug commands can be written in a single line using | separators
+  Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'
+
+  " On-demand loading
+  Plug 'preservim/nerdtree', { 'on': 'NERDTreeToggle' }
+  Plug 'tpope/vim-fireplace', { 'for': 'clojure' }
+
+  " Using a non-default branch
+  Plug 'rdnetto/YCM-Generator', { 'branch': 'stable' }
+
+  " Using a tagged release; wildcard allowed (requires git 1.9.2 or above)
+  Plug 'fatih/vim-go', { 'tag': '*' }
+
+  " Plugin options
+  Plug 'nsf/gocode', { 'tag': 'v.20150303', 'rtp': 'vim' }
+
+  " Plugin outside ~/.vim/plugged with post-update hook
+  Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+
+  " Unmanaged plugin (manually installed and updated)
+  Plug '~/my-prototype-plugin'
+
+  " Deoplete "
+  Plug 'deoplete-plugins/deoplete-clang'
+  " Initialize plugin system
+  " - Automatically executes `filetype plugin indent on` and `syntax enable`.
+  
+  "autoclose.nvim"
+  Plug 'm4xshen/autoclose.nvim'
+  call plug#end()
+
+  let g:python3_host_prog = '/Users/max/.pyenv/versions/3.11.5/bin/python'
+  " You can revert the settings after the call like so:
+  "   filetype indent off   " Disable file-type-specific indentation
+  "   syntax off            " Disable syntax highlighting
+
+  "REMAPS"
+  nnoremap <C-j> :m .+1<CR>==
+  nnoremap <C-k> :m .-2<CR>==
+  inoremap <C-j> <Esc>:m .+1<CR>==gi
+  inoremap <C-k> <Esc>:m .-2<CR>==gi
+  vnoremap <C-j> :m '>+1<CR>gv=gv
+  vnoremap <C-k> :m '<-2<CR>gv=gv
+]])
+
+-- User requires
+require("autoclose").setup()
+
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
